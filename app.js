613e9942b262d07581f9a8d56843f5aa7bc7748a
eternale88 <ethanrobbins@ymail.com>
2018-03-26 09:46:13 +0800

    added process.env.PORT to app.listen

--- app.js
+++ app.js
@@ -51,6 +51,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, function(){
-   console.log("The YelpCamp Server Has Started!");
+app.listen(process.env.PORT || 3000, function(){
+ console.log('yelpcamp is running at port 3000...');
 });
